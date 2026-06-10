dc043ebba0a19c81eca5811ab7a0cfc8dca595db
rob-scape <robertheel@web.de>
2026-06-10 15:27:41 +0200

    Update social.js

--- assets/socials.js
+++ assets/socials.js
@@ -1,8 +1,8 @@
 document.getElementById('site-socials').innerHTML = `
   <div class="socials">
-    <a href="https://www.instagram.com/robert.heel" title="Instagram" target="_blank" rel="noopener noreferrer"></a>
-    <a href="https://robertheel.bandcamp.com" title="Bandcamp" target="_blank" rel="noopener noreferrer"></a>
-    <a href="https://soundcloud.com/robertheel" title="Soundcloud" target="_blank" rel="noopener noreferrer"></a>
-    <a href="https://github.com/rob-scape" title="GitHub" target="_blank" rel="noopener noreferrer"></a>
+    <a href="https://www.instagram.com/robert.heel" title="Instagram" aria-label="Instagram" target="_blank" rel="noopener noreferrer"></a>
+    <a href="https://robertheel.bandcamp.com" title="Bandcamp" aria-label="Bandcamp" target="_blank" rel="noopener noreferrer"></a>
+    <a href="https://soundcloud.com/robertheel" title="Soundcloud" aria-label="Soundcloud" target="_blank" rel="noopener noreferrer"></a>
+    <a href="https://github.com/rob-scape" title="GitHub" aria-label="GitHub" target="_blank" rel="noopener noreferrer"></a>
   </div>
 `;
