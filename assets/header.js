41b70da727ab4364b7b2abb8f23d3c53f80ba2c8
rob-scape <robertheel@web.de>
2026-06-10 14:56:31 +0200

    upload html files

---
+++ assets/header.js
@@ -0,0 +1,12 @@
+document.getElementById('site-header').innerHTML = `
+  <header>
+    <div class="logo">
+      <a href="${BASE}index.html">
+        <img src="${BASE}assets/1_rh_instruct-600-600-invert.png" alt="logo">
+      </a>
+    </div>
+    <nav>
+      <a href="${BASE}about.html">about</a>
+    </nav>
+  </header>
+`;
---
+++ assets/socials.js
@@ -0,0 +1,8 @@
+document.getElementById('site-socials').innerHTML = `
+  <div class="socials">
+    <a href="https://www.instagram.com/robert.heel" title="Instagram" target="_blank" rel="noopener noreferrer"></a>
+    <a href="https://robertheel.bandcamp.com" title="Bandcamp" target="_blank" rel="noopener noreferrer"></a>
+    <a href="https://soundcloud.com/robertheel" title="Soundcloud" target="_blank" rel="noopener noreferrer"></a>
+    <a href="https://github.com/rob-scape" title="GitHub" target="_blank" rel="noopener noreferrer"></a>
+  </div>
+`;
